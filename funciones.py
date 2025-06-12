13c7d7b733b757c3f506ec9079b671e349a34f9b
marianoou207 <cortinas-sonrisita.5i@icloud.com>
2025-06-12 00:57:35 -0300

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,56 @@
+def validar_entero(mensaje):
+    while True:
+        valor = input(mensaje)
+        if valor.isdigit():
+            return int(valor)
+        else:
+            print("Error: ingrese solo números.")
+
+def cargar_stock(INSUMOS:list, DEPOSITOS:list) -> int:
+    print("Carga de stock")
+    stock = [[0 for _ in range(len(INSUMOS))] for _ in range(len(DEPOSITOS))]
+    for i in range(len(DEPOSITOS)):
+        print(f"Depósito: {DEPOSITOS[i]}:")
+        for j in range(len(INSUMOS)):
+            cantidad = validar_entero(f"Ingrese la cantidad de {INSUMOS[j]}: ")
+            stock[i][j] = cantidad
+    print("Matriz de stock cargada:")
+    mostrar_matriz(stock, INSUMOS, DEPOSITOS)
+    return stock
+
+def mostrar_matriz(matriz, INSUMOS, DEPOSITOS):
+    print("Depósito\\insumos    ", *INSUMOS)
+    for i in range(len(DEPOSITOS)):
+        fila = DEPOSITOS[i] + "             "
+        for j in range(len(INSUMOS)):
+            fila += str(matriz[i][j]) + "           "
+        print(fila)
+
+def mostrar_depositos(stock:int,INSUMOS:list, DEPOSITOS:list, minimo=5000) -> str:
+    print(f"Depósitos con stock total superior a {minimo} unidades:")
+    for i in range(len(DEPOSITOS)):
+        total = 0
+        for j in range(len(INSUMOS)):
+            total += stock[i][j]
+        if total > minimo:
+            print(f"El stock superior a {minimo} unidades en el depósito {DEPOSITOS[i]} es de: {total} unidades")
+
+def mostrar_stock_total(stock:int,INSUMOS:list, DEPOSITOS:list, minimo=3000) -> str: 
+    print(f"Insumos con stock total superior a {minimo} unidades:")
+    for i in range(len(INSUMOS)):
+        total = 0
+        for j in range(len(DEPOSITOS)):
+            total += stock[j][i]
+        if total > minimo:
+            print(f"El insumo {INSUMOS[i]} tiene un stock total de {total} unidades")
+
+def identificar_deposito_mayor_stock(stock:int, INSUMOS:list, DEPOSITOS:list) -> str:
+    print("Depósitos con mayor stock de insumos:")
+    for j in range(len(INSUMOS)):
+        maximo = stock[0][j]
+        indice = 0
+        for i in range(len(DEPOSITOS)):
+            if stock[i][j] > maximo:
+                maximo = stock[i][j]
+                indice = i
+        print(f"El depósito con mayor stock de {INSUMOS[j]} es {DEPOSITOS[indice]} con un máximo de {maximo} unidades")
